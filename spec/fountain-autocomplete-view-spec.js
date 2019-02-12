cfa0bb7b654a84193773b4727c0f714145b098d8
Ben Sobel <7thof14217@gmail.com>
2019-02-12 01:59:37 -0800

    Initial Commit!

---
+++ spec/fountain-autocomplete-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FountainAutocompleteView from '../lib/fountain-autocomplete-view';
+
+describe('FountainAutocompleteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
